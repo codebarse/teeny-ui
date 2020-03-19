3c0347b5d0aba8f8aad9f237b719a21f68f180dc
pulkitkariryaa <pulkitkariryaa@gmail.com>
2020-03-20 01:34:20 +0530

    Adding redirect and gcp url

---
+++ src/component/TeenyRedirect.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+class TeenyRedirect extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      value: "",
+      teenyUrl: ""
+    };
+  }
+
+  render() {
+    return(
+      <div>Redirect</div>
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import TeenyUI from './component/TeenyUI';
+import TeenyRedirect from './component/TeenyRedirect';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 
-ReactDOM.render( < TeenyUI / > , document.getElementById('container'));
+export default function App() {
+  return (
+    <Router>
+      <div>
+        <Switch>
+          <Route path="/">
+            <TeenyRedirect />
+          </Route>
+          <Route path="/about">
+            <TeenyUI />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+ReactDOM.render(< App />, document.getElementById('container'));
